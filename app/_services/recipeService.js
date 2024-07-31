72063f081a2a93b8660cc19ed463c5cfe4f19061
sunggeorge <sunggeorge@gmail.com>
2024-07-30 23:42:30 -0600

    merge pending services

---
+++ app/_services/recipeService.js
@@ -0,0 +1,27 @@
+import { db } from "../_utils/firebase";
+import { collection, addDoc, updateDoc, doc, arrayUnion } from "from 'firebase/firestore/lite'";
+
+export const addRecipe = async (userId, recipeData) => {
+  const recipesRef = collection(db, "recipes");
+  const newRecipe = await addDoc(recipesRef, {
+    ...recipeData,
+    authorID: userId,
+    likes: 0,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    reviews: []
+  });
+
+  const userRef = doc(db, "users", userId);
+  await updateDoc(userRef, {
+    createdRecipes: arrayUnion(newRecipe.id)
+  });
+};
+
+export const updateRecipe = async (recipeId, updatedData) => {
+  const recipeRef = doc(db, "recipes", recipeId);
+  await updateDoc(recipeRef, {
+    ...updatedData,
+    updatedAt: new Date()
+  });
+};
